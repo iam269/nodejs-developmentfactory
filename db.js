c3f14681cf0d88fdfee95c3b0aa189e86a1bfdc5
Aurel Ionita <ionitaaurel32@gmail.com>
2026-01-21 23:37:42 +0200

    course finished

---
+++ db.js
@@ -0,0 +1,6 @@
+function connect(){
+    console.log("Connected to the database.");
+    return "db.json";
+}
+
+module.exports = {connect};
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 /* console.log(global.process.env); */
 
+//create a simple server using http module
+
 const http = require('http');
+const db = require('./db').connect();
 
 const host =  process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
@@ -8,9 +11,60 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  switch (req.url) {
+    case '/':
+      res.end('Home Page');
+      break;
+    case '/about':
+      res.end('About Page');
+      break;
+    case '/contact':
+      res.end('Contact Page');
+      break;
+    default:
+      res.statusCode = 404;
+      res.end('Page Not Found');
+      break;
+  }
 });
 
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
+
+
+//reading file using fs module
+
+// const fs = require('fs');
+
+// fs.readFile("test.txt", (error, data) => {
+//   if (error) {
+//     console.error(error);
+//     return;
+//   }
+//   console.log(data.toString());
+// })
+
+// fs.writeFile("test.txt", "Salutare din NodeJS!",{flag: "a"}, (error) => {
+//   if (error) {
+//     console.error(error);
+//     return;
+//   }
+//   console.log("File written successfully!");
+// });
+
+// fs.rename("test.txt", "node.txt", (error) => {
+//   if (error) {
+//     console.error(error);
+//     return;
+//   }
+//   console.log("File renamed successfully!");
+// });
+
+// fs.unlink("node.txt", (error) => {
+//   if (error) {
+//     console.error(error);
+//     return;
+//   }
+//   console.log("File deleted successfully!");
+// });
